8561857824ea7397e19612fae156a0498aba4574
LEELARANIALURU <76864260+LEELARANIALURU@users.noreply.github.com>
2022-05-17 21:15:28 +0530

    Update Model.py

--- Model.py
+++ Model.py
@@ -1,6 +1,4 @@
-#importing libraries
 from tensorflow import keras
-import matplotlib.pyplot as plt
 import numpy as np
 
 #loading dataset
@@ -13,6 +11,9 @@ x_test = x_test/255
 model = keras.Sequential([
               keras.layers.Flatten(input_shape = (28,28)),
               keras.layers.Dense(100, activation='relu'),
+              keras.layers.Dense(50, activation='relu'),
+              keras.layers.Dense(20, activation='relu'),
+              keras.layers.Dense(15, activation='relu'),
               keras.layers.Dense(10, activation = 'softmax')
 ])
 
@@ -23,10 +24,34 @@ model.compile(
 )
 
 #fitting data
-model.fit(x_train, y_train, epochs = 10)
+model.fit(x_train, y_train, epochs = 20)
 
 #evaluation
 model.evaluate(x_test, y_test)
 
 #making predictions
 y_predicted = model.predict(x_test)
+
+model.save("hand_writ_digi.h5")
+
+#testing on custom data
+import cv2
+
+img = cv2.imread(r"C:\Users\HP\Desktop\test_img8.jpg")
+
+img = cv2.resize(img,(28,28))
+
+img = np.array(img)
+
+img = img[:,:,0]
+
+img = img.reshape(1,28,28,1)
+img = img/255.0
+res = model.predict([img])[0]
+print(np.argmax(res))
+
+#reading out the output
+import pyttsx3
+pyobj = pyttsx3.init()
+pyobj.say(np.argmax(res))
+pyobj.runAndWait()
