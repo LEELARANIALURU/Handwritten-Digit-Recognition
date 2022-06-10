28038fde4b006d28c3ea6af280d0d2e50df13ad8
LEELARANIALURU <76864260+LEELARANIALURU@users.noreply.github.com>
2022-06-11 00:41:44 +0530

    Update Model.py

--- Model.py
+++ Model.py
@@ -1,57 +1,90 @@
-from tensorflow import keras
-import numpy as np
+import tensorflow as tf
 
-#loading dataset
-(x_train, y_train) , (x_test, y_test) = keras.datasets.mnist.load_data()
+#Loading Data
 
-x_train = x_train/255
-x_test = x_test/255
+df = tf.keras.datasets.mnist.load_data()
 
-#building the model
-model = keras.Sequential([
-              keras.layers.Flatten(input_shape = (28,28)),
-              keras.layers.Dense(100, activation='relu'),
-              keras.layers.Dense(50, activation='relu'),
-              keras.layers.Dense(20, activation='relu'),
-              keras.layers.Dense(15, activation='relu'),
-              keras.layers.Dense(10, activation = 'softmax')
+(X_train,y_train),(X_test,y_test)=df
+
+# Normalization
+
+X_train=(X_train.astype('float32').reshape(X_train.shape[0],28,28,1))/255.0
+X_test=(X_test.astype('float32').reshape(X_test.shape[0],28,28,1))/255.0
+
+
+# Building the model
+
+from tensorflow.keras.layers import BatchNormalization
+model=tf.keras.models.Sequential([
+  
+  # Layer1
+  tf.keras.layers.Conv2D(32,(3,3),activation='relu',input_shape=(28,28,1)),
+  tf.keras.layers.MaxPooling2D(2,2),
+  BatchNormalization(axis=-1),
+  
+  # Layer2
+  tf.keras.layers.Conv2D(64,(3,3),activation='relu',input_shape=(28,28,1)),
+  tf.keras.layers.MaxPooling2D(2,2),
+  BatchNormalization(axis=-1),
+  
+  #Flattening
+  tf.keras.layers.Dropout(0.3),
+  tf.keras.layers.Flatten(),
+  
+  #Dense Layer
+  tf.keras.layers.Dense(128,activation='relu'),
+  BatchNormalization(axis=-1),
+  
+  #Output Layer
+  tf.keras.layers.Dense(10,activation='softmax')
 ])
 
-model.compile(
-    optimizer = 'adam',
-    loss = 'sparse_categorical_crossentropy',
-    metrics = ['accuracy']
-)
+model.summary()
 
-#fitting data
-model.fit(x_train, y_train, epochs = 20)
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics= ['accuracy'])
 
-#evaluation
-model.evaluate(x_test, y_test)
+#Fitting Data into the model
+h = model.fit (X_train, y_train , epochs= 10,validation_split=0.2)
 
-#making predictions
-y_predicted = model.predict(x_test)
+#saving the model
+model.save("handwritten_digit_recognition_model.h5")
 
-model.save("hand_writ_digi.h5")
+#Training Accuracy
+loss,acc=model.evaluate(X_train,y_train, verbose =0)
+print("The train accuracy of model is :-",acc*100)
 
-#testing on custom data
+#Testing Accuracy
+test_loss,test_acc=model.evaluate(X_test,y_test,verbose =0)
+print("The test accuracy of model is :-",test_acc*100)
+
+# Loading saved model
+#model = tf.keras.models.load_model(r'path\handwritten_digit_recognition_model.h5')
+
+# Making prediction
 import cv2
+import numpy as np
 
-img = cv2.imread(r"C:\Users\HP\Desktop\test_img8.jpg")
-
+img = cv2.imread(r"path\test_img12.jpg")
 img = cv2.resize(img,(28,28))
-
 img = np.array(img)
-
 img = img[:,:,0]
-
 img = img.reshape(1,28,28,1)
 img = img/255.0
 res = model.predict([img])[0]
 print(np.argmax(res))
 
-#reading out the output
+# Reading out the output
 import pyttsx3
 pyobj = pyttsx3.init()
 pyobj.say(np.argmax(res))
 pyobj.runAndWait()
+
+# Plotting Accuracy and Loss
+df0=pd.DataFrame(x.history)
+plt.figure(figsize = (7,7))
+sns.lineplot(data=df0[['accuracy','val_accuracy']],palette=['r', 'g'],linewidth=4)
+plt.show()
+
+plt.figure(figsize = (7,7))
+sns.lineplot(data=df0[['loss','val_loss']],palette=['b', 'k'],linewidth=4)
+plt.show()
